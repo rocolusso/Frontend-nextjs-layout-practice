2941a9bf9808f6064979b3ce2ab508014237a19e
ysphotodev <ysphotodev@gmail.com>
2024-09-22 23:53:44 +0300

    setted initial prettier config

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,7 +1,6 @@
-import React from 'react';
-import {
-  Html, Head, Main, NextScript,
-} from 'next/document';
+import React from "react";
+
+import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
